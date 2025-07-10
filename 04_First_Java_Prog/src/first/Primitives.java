543da951675af312fcafae26c44d79f56b28e0b9
Somya Keshri <keshrisomya9596@gmail.com>
2025-07-10 21:40:43 +0530

    Primitives

---
+++ 04_First_Java_Prog/src/first/Primitives.java
@@ -0,0 +1,16 @@
+package first;
+
+public class Primitives {
+    public static void main(String[] args) {
+
+        // String is not primitive
+
+        int rollno = 65;                // 4 bytes
+        char letter = 's';               // 1 byte
+        float marks = 95.67f;             // 4 bytes
+        double largeDecimalNo = 254876513.84;       // 8 bytes
+        long largeInteger = 215876564985312L;        // 4 or 8 bytes
+        boolean check = false;
+
+    }
+}
