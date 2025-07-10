b526bc67a0ffd01aca20191f136b6c951f4e69c3
Somya Keshri <keshrisomya9596@gmail.com>
2025-07-10 21:09:49 +0530

    taking input

--- 04_First_Java_Prog/src/first/Name.java
+++ 04_First_Java_Prog/src/first/Name.java
@@ -1,8 +1,13 @@
 package first;
 
+import java.util.Scanner;
+
 public class Name {
     public static void main(String[] args) {
-        System.out.println(84651);
-        System.out.println("My name is Somya");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextInt());
+
+        Scanner line = new Scanner(System.in);
+        System.out.println(line.nextLine());
     }
 }
