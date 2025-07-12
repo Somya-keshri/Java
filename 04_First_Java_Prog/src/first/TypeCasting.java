a9d9cb124b2c6b8556c3f1b301dcdd6786d15fde
Somya Keshri <keshrisomya9596@gmail.com>
2025-07-12 20:32:00 +0530

    type_casting

--- 04_First_Java_Prog/src/first/TypeCasting.java
+++ 04_First_Java_Prog/src/first/TypeCasting.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class TypeCasting {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
 //        int num = input.nextFloat();    // wrong  in int we cant store float
 
 //        float num = input.nextInt();      // in float we can store int
@@ -14,9 +15,37 @@ public class TypeCasting {
         System.out.println(no);       // type casting
 
         // Automatic type promotion in expressions
-        int a = 250;
-        byte b = (byte)(a);     // 257 % 256 = 1
+        int a = 257;
+        byte n = (byte)(a);     // 257 % 256 = 1
 
-        System.out.println(b);
+        System.out.println(n);      // 1
+
+        byte p = 40;
+        byte q = 50;
+        byte r = 100;
+        int t = (p * q) / r;
+
+        System.out.println(t);         // 20
+
+//        byte m = 50;
+//        m = m * 20;         // this will give error (byte can't store that much value)
+
+        int number = 'A';
+        System.out.println(number);   // ASCII value
+        // Java follow unicode value
+
+        System.out.println(3 * 5.6f);
+
+        byte b = 42;
+        char c = 'a';
+        short s = 1024;
+        int i = 50000;
+        float f = 5.67f;
+        double d = 0.1234;
+        double result = (f * b) + (i / c) - (d * s);
+        // float + int - double = double
+        // smaller value converted to bigger value
+        System.out.println((f * b) + "  |  " + (i / c) + "  |  " + (d * s));
+        System.out.println(result);
     }
 }
