d2fd39eecc4d268acdea45d4da949daaf18bdc2e
Somya Keshri <keshrisomya9596@gmail.com>
2025-07-15 02:26:11 +0530

    sum

--- 06_Methods/src/code/sum_01.java
+++ 06_Methods/src/code/sum_01.java
@@ -4,7 +4,29 @@ import java.util.Scanner;
 
 public class sum_01 {
     public static void main(String[] args){
-        sum();
+//        int ans = sum2();
+//        System.out.println(ans);
+
+        int ans = sum3(20, 30);
+        System.out.println(ans);
+    }
+
+    // pass the value of numbers when you are calling the method in main()
+    static int sum3(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    // return the value
+    static int sum2(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter num1: ");
+        int num1 = in.nextInt();
+        System.out.print("Enter num2: ");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        return sum;
+//        System.out.println("Unreachable statement");
     }
 
     static void sum(){
