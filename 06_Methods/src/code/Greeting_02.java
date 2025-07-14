a88ac1e3173d882e379ae4dee81d7973f4859c9d
Somya Keshri <keshrisomya9596@gmail.com>
2025-07-15 02:29:28 +0530

    greet

---
+++ 06_Methods/src/code/Greeting_02.java
@@ -0,0 +1,10 @@
+package code;
+
+public class Greeting_02 {
+    public static void main(String[] args) {
+        greeting();
+    }
+    static void greeting() {
+        System.out.println("Hello World!!");
+    }
+}
