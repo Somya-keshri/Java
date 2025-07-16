f0eb0a569badc03906807ab32bfaeb73332141ec
Somya Keshri <keshrisomya9596@gmail.com>
2025-07-16 16:34:14 +0530

    function

--- 07_Arrays/src/code/PassingInFunc.java
+++ 07_Arrays/src/code/PassingInFunc.java
@@ -7,9 +7,11 @@ public class PassingInFunc {
         int[] nums = {3, 5, 2, 10};
         System.out.println(Arrays.toString(nums));       // [3, 5, 2, 10]
         change(nums);
-        System.out.println(Arrays.toString(nums));       // [3, 47, 2, 10]
+        System.out.println(Arrays.toString(nums));       // [3, 47, 2, 10]  (change the original array)
     }
     static void change(int[] arr){
         arr[1] = 47;
     }
+    // String are immutable whereas Arrays are mutable in java
+    // mutable basically means change the object
 }
