257e6c8552206a023f6894c3f335dfe990196eeb
Somya Keshri <keshrisomya9596@gmail.com>
2025-09-01 21:29:51 +0530

    bubble sort

---
+++ 10_Sorting/src/code/BS/BubbleS.java
@@ -0,0 +1,27 @@
+package code.BS;
+
+import java.util.Arrays;
+
+public class BubbleS {
+    public static void main(String[] args) {
+        int[] arr = {5, 8, 2, 4, 3};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubble(int[] arr){
+        // run the steps n-1 times
+        for (int i = 0; i < arr.length; i++) {
+            // for each step, max item will come at the last respective index
+            for (int j = 1; j < arr.length - i; j++) {
+                // swap if the item is smaller than the previous item
+                if(arr[j] < arr[j-1]){
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+    }
+}
