1a32da53551b547a10fcf1fe54e02091ab1e895f
Somya Keshri <keshrisomya9596@gmail.com>
2025-09-03 02:47:05 +0530

    selection

---
+++ 10_Sorting/src/SelectionSort/SelectionS.java
@@ -0,0 +1,34 @@
+package SelectionSort;
+
+import java.util.Arrays;
+
+public class SelectionS {
+    public static void main(String[] args) {
+        int[] arr = {3, -1, 5, 17, 4, 2};
+        System.out.println(arr.length);
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void selection(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+
+            swap(arr, maxIndex, last);
+        }
+    }
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+    static int getMaxIndex(int[] arr, int start, int end){
+        int max = start;
+        for (int i = start; i<=end; i++){
+            if(arr[max] < arr[i]){
+                max = i;
+            }
+        }
+        return max;
+    }
+}
