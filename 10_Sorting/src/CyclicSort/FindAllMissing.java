25ed0825c3b0ae342b84cfd080dfca588ae720f9
Somya Keshri <keshrisomya9596@gmail.com>
2025-09-09 02:05:50 +0530

    cyclic

---
+++ 10_Sorting/src/CyclicSort/FindAllMissing.java
@@ -0,0 +1,32 @@
+package CyclicSort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class FindAllMissing {
+    public List<Integer> findDisappearedNum(int[] nums){
+        int i = 0;
+        while(i < nums.length){
+            int correct = nums[i] - 1;
+            if(nums[i] != nums[correct]){
+                swap(nums, i, correct);
+            }
+            else {
+                i++;
+            }
+        }
+        // just find missing num
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < nums.length; index++) {
+            if(nums[index] != index){
+                ans.add(index + 1);
+            }
+        }
+        return ans;
+    }
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
