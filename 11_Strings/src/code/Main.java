af73410f1b17b90538593323f8bdbbf8cd780275
Somya Keshri <keshrisomya9596@gmail.com>
2025-09-09 20:14:08 +0530

    string

---
+++ 11_Strings/src/code/Main.java
@@ -0,0 +1,17 @@
+package code;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 3, 4, 7};
+        int num = 10;
+        String name = "Harry Potter";
+        System.out.println(name);
+
+        String a = "Harry";
+        System.out.println(a);
+        String b = "Harry";
+        System.out.println(b);
+        a = "Potter";
+        System.out.println(a);
+    }
+}
