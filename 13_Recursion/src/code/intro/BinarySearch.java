1b3a736486cfe96e45254af1d9b3233c0924f413
Somya Keshri <keshrisomya9596@gmail.com>
2025-09-11 18:47:55 +0530

    binary

---
+++ 13_Recursion/src/code/intro/BinarySearch.java
@@ -0,0 +1,22 @@
+package code.intro;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 66, 77};
+        int target = 77;
+        System.out.println(search(arr, target, 0, arr.length-1));
+    }
+    static int search(int[] arr, int target, int s, int e){
+        if(s > e){
+            return -1;
+        }
+        int m = s + (e - s)/2;
+        if(target == arr[m]){
+            return m;
+        }
+        if (target < arr[m]){
+            return search(arr, target, s, m-1);
+        }
+        return search(arr, target, m+1, e);
+    }
+}
