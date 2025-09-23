c856afdadcd775f99e6b2a1aac364cfd99239e94
Somya Keshri <keshrisomya9596@gmail.com>
2025-09-23 21:43:00 +0530

    fibonacci

---
+++ 14_Time_Space_Complexity/src/code/Fibo.java
@@ -0,0 +1,20 @@
+package code;
+
+public class Fibo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 11; i++) {
+            System.out.println(fiboFormula(i));
+        }
+        System.out.println();
+        System.out.println("15th fibonacci: " + fiboFormula(15));
+        System.out.println(fiboFormula(50));
+    }
+    static long fiboFormula(int n){
+        // base condition
+        if (n < 2) {
+            return n;
+        }
+        // long or int
+        return (long)((Math.pow(((1 + Math.sqrt(5)) / 2), n) - Math.pow(((1 - Math.sqrt(5)) / 2), n)) / Math.sqrt(5));
+    }
+}
