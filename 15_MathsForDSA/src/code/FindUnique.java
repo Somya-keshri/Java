9c54813bdf4354f6bcc32dec7c687010c25d69cd
Somya Keshri <keshrisomya9596@gmail.com>
2025-09-28 23:01:47 +0530

    XOR

---
+++ 15_MathsForDSA/src/code/FindUnique.java
@@ -0,0 +1,15 @@
+package code;
+
+public class FindUnique {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 3, 4, 6, 2, 4};
+        System.out.println(ans(arr));
+    }
+    private static int ans(int[] arr){
+        int unique = 0;
+        for(int n: arr){
+            unique ^= n;     // XOR
+        }
+        return unique;
+    }
+}
