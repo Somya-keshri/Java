5d4412a8d0a3a1493af96185304c3271450b79f2
Somya Keshri <keshrisomya9596@gmail.com>
2025-09-29 23:42:06 +0530

    set bits

---
+++ 15_MathsForDSA/src/code/SetBits.java
@@ -0,0 +1,30 @@
+package code;
+
+import java.util.Scanner;
+
+public class SetBits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        System.out.println(Integer.toBinaryString(n));
+        System.out.println(setBits(n));
+    }
+    private static int setBits(int n){
+        int count = 0;
+        // n & -n → extracts the rightmost set bit of n
+
+        while(n > 0){
+            count++;
+            n -= (n & -n);     // remove the rightmost set bit
+        }
+
+        // OR
+//        while(n > 0){
+//            count++;
+//            n = n & (n-1);
+//        }
+        return count;
+    }
+}
