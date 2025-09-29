db963f2670d38a14792949a5f4b33fb42ec60114
Somya Keshri <keshrisomya9596@gmail.com>
2025-09-29 19:51:25 +0530

    sum

---
+++ 15_MathsForDSA/src/code/PascalsTriangleSum.java
@@ -0,0 +1,13 @@
+package code;
+
+public class PascalsTriangleSum {
+    public static void main(String[] args) {
+        int n = 5;
+
+        //Formula: Sum of nth row of Pascal's Triangle = 2^(n-1)
+        // Example: Row 5 → 1 4 6 4 1 → sum = 16 → 2^(5-1) = 16
+
+        int sum = 1 << (n-1);   // left shift
+        System.out.println("Sum of " + n + "th row in Pascals Triangle = " + sum);
+    }
+}
