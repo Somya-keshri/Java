298b8286b8dc3f5a87d4eba24f450e19ca164bc5
Somya Keshri <keshrisomya9596@gmail.com>
2025-09-29 21:30:20 +0530

    power

---
+++ 15_MathsForDSA/src/code/PowerOfTwo.java
@@ -0,0 +1,20 @@
+package code;
+
+import java.util.Scanner;
+
+public class PowerOfTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number n: ");
+        int n = sc.nextInt();
+
+        boolean ans;
+        if(n == 0){
+            ans = false;     // 0 is not a power of 2
+        }
+        else {
+            ans = (n & (n - 1)) == 0;
+        }
+        System.out.println(ans);
+    }
+}
