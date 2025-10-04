0d7caacb16d424f08dba13fa3bb4b9bbb21ff40e
Somya Keshri <keshrisomya9596@gmail.com>
2025-10-04 14:59:45 +0530

    no of steps

---
+++ 13_Recursion/src/code/easy/CountSteps.java
@@ -0,0 +1,21 @@
+package code.easy;
+
+public class CountSteps {
+    public static void main(String[] args) { 
+        System.out.println(numberOfSteps(35));
+    }
+
+    static int numberOfSteps(int num) {
+        return helper(num, 0);
+    }
+
+    static int helper(int num, int steps) {
+        if (num == 0) {
+            return steps;
+        }
+        if (num % 2 == 0) {
+            return helper(num/2, steps+1);
+        }
+        return helper(num-1, steps+1);
+    }
+}
