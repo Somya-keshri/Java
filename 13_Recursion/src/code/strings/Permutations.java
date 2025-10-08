fc10dab66d3fc9cd6bb8d57a6b0b6c9c69d1d9ad
Somya Keshri <keshrisomya9596@gmail.com>
2025-10-08 16:59:13 +0530

    permutations

---
+++ 13_Recursion/src/code/strings/Permutations.java
@@ -0,0 +1,65 @@
+package code.strings;
+
+import java.util.ArrayList;
+
+public class Permutations {
+    public static void main(String[] args) {
+        permutation("", "abc");
+
+        ArrayList<String> ans = permutationsList("", "abc");
+        System.out.println(ans);
+
+        System.out.println("Permutations count: " + permutationsCount("", "abc"));
+    }
+
+    static void permutation(String p,String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+
+        for(int i=0; i<=p.length(); i++){
+            String first = p.substring(0, i);
+            String second = p.substring(i, p.length());
+
+            permutation(first+ch+second, up.substring(1));
+        }
+    }
+
+    static ArrayList<String> permutationsList(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+
+        // local to this call
+        ArrayList<String> ans = new ArrayList<>();
+
+        for(int i=0; i<=p.length(); i++){
+            String f = p.substring(0, i);
+            String s = p.substring(i, p.length());
+
+            ans.addAll(permutationsList(f+ch+s, up.substring(1)));
+        }
+        return ans;
+    }
+
+    static int permutationsCount(String p, String up){
+        if(up.isEmpty()){
+            return 1;
+        }
+        int count = 0;
+        char ch = up.charAt(0);
+
+        for(int i=0; i<=p.length(); i++){
+            String f = p.substring(0, i);     // before where you insert the new character
+            String s = p.substring(i, p.length());    // after where you insert the new character
+
+            count = count + permutationsCount(f+ch+s, up.substring(1));
+        }
+        return count;
+    }
+}
