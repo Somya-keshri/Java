ffa38a1233e00aa203d01c2ca739ff30c82cc000
Somya Keshri <keshrisomya9596@gmail.com>
2026-07-05 21:25:55 +0530

    floyd’s triangle

---
+++ 12_Patterns/src/code/Ass13.java
@@ -0,0 +1,25 @@
+package code;
+
+import java.util.Scanner;
+
+public class Ass13 {
+    public static void main(String[] args) {
+        // Floyd’s Triangle Pattern
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter num of rows: ");
+        int n = sc.nextInt();
+        int m=1;
+
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+
+                System.out.print(m + " ");
+                m++;
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
