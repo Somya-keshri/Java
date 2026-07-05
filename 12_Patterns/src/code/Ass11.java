0bc929a6b39308cbe56f4ef1ceef8e330fecb515
Somya Keshri <keshrisomya9596@gmail.com>
2026-07-05 21:01:31 +0530

    symmetric number pyramid

---
+++ 12_Patterns/src/code/Ass11.java
@@ -0,0 +1,30 @@
+package code;
+
+import java.util.Scanner;
+
+public class Ass11 {
+    public static void main(String[] args) {
+        // SYMMETRIC NUMBER PYRAMID PATTERN
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter num of rows: ");
+        int n = sc.nextInt();
+
+        for (int row = 1; row <= n; row++) {
+            for (int space = 1; space <= n-row; space++) {
+                System.out.print("  ");
+            }
+            for (int col = 1; col <= 2*row-1; col++) {
+                if(col<=n){
+                    System.out.print(col + " ");
+                }
+                else {
+                    System.out.print(2*row-col + " ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
