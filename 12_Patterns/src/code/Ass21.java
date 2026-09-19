a57610b5c46d6e78ff4a67d97f00d04803690d6f
Somya Keshri <keshrisomya9596@gmail.com>
2026-09-19 16:10:00 +0530

    incomplete solution

--- 12_Patterns/src/code/Ass21.java
+++ 12_Patterns/src/code/Ass21.java
@@ -1,4 +1,15 @@
 package code;
 
+import java.util.Scanner;
+
 public class Ass21 {
+    public static void main(String[] args) {
+        // SPIRAL MATRIX PATTERN
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter num of rows: ");
+        int n = sc.nextInt();
+
+        
+    }
 }
